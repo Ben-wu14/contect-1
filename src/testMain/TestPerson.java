ef6a1ec3393d5449424c943f6a6813131a7c0ced
84568 <845687624@qq.com>
2017-03-03 22:42:28 +0800

    Add a new java file for testing Person get/set funtion

---
+++ src/testMain/TestPerson.java
@@ -0,0 +1,31 @@
+package testMain;
+import contectCore.*;
+public class TestPerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person ben_ten = new Person("ben_ten");
+		ben_ten.setPhoneNumber("13660081839");
+		ben_ten.setTel("87865467");
+		ben_ten.setCompany("SCAU");
+		ben_ten.setPostCode("510655");
+		ben_ten.setNote("It's a cartoon charactor");
+		ben_ten.setAddress("China X-man school street");
+		ben_ten.setPicture("\\picture\\ben_ten.jpg");
+		Time date = new Time(1996, 9, 30);
+		ben_ten.setBirthday(date);
+		ben_ten.setEmail("845687624@qq.com");
+		ben_ten.setInstantContect("phone");
+		System.out.println("Phone:"+ben_ten.getPhoneNumber());
+		System.out.println("Tel:"+ben_ten.getTel());
+		System.out.println("Company"+ben_ten.getCompany());
+		System.out.println("PostCode"+ben_ten.getPostCode());
+		System.out.println("Note:"+ben_ten.getNote());
+		System.out.println("Address"+ben_ten.getAddress());
+		System.out.println("Picture link:"+ben_ten.getPicture());
+		System.out.println("Birthday:"+ben_ten.getBirthday().toString());
+		System.out.println("Email:"+ben_ten.getEmail());
+		System.out.println("Instant contect:"+ben_ten.getInstantContect());
+	}
+
+}
