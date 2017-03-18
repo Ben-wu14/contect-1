834fe26bd10e7c676428caf3efa8b21f22cdbe8f
ben <845687624@qq.com>
2017-03-18 10:43:28 +0800

    更新full list测试程序

--- src/test_FullLIst/TestFullList.java
+++ src/test_FullLIst/TestFullList.java
@@ -1,7 +1,28 @@
 package test_FullLIst;
 
+import contectCore.FullList;
+import contectCore.Person;
+
 public class TestFullList {
 	public static void main(String[] args) {
-		
+		FullList fullList = new FullList();
+		fullList.addNewPerson(new Person("ben"));
+		fullList.addNewPerson(new Person("ten"));
+		fullList.addNewPerson(new Person("bill"));
+		fullList.addNewPerson(new Person("xian"));
+		fullList.addNewPerson(new Person("Œ‚“‡∑≤"));
+		fullList.addNewPerson(new Person("Œ‚∂´…˝"));
+		fullList.addNewPerson(new Person("123ƒæÕ∑»À"));
+		fullList.addNewPerson(new Person("apple"));
+		fullList.addNewPerson(new Person("Tom"));
+		fullList.addNewPerson(new Person("∞¢“Ã"));
+		fullList.addNewPerson(new Person("xj"));
+		fullList.addNewPerson(new Person("Œ‚ºŒ»Û"));
+		for (Person person : fullList.getPersonList()) {
+			System.out.println(person.getName());
+		}
+		for (int person : fullList.getHeader()) {
+			System.out.println(person);
+		}
 	}
 }
