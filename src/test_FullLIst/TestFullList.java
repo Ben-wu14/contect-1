b0a1897a2b725dcc972a7b36577d0cf58a962573
ben <845687624@qq.com>
2017-03-18 19:19:41 +0800

    fullist添加删除程序的测试

--- src/test_FullLIst/TestFullList.java
+++ src/test_FullLIst/TestFullList.java
@@ -6,7 +6,8 @@ import contectCore.Person;
 public class TestFullList {
 	public static void main(String[] args) {
 		FullList fullList = new FullList();
-		fullList.addNewPerson(new Person("ben"));
+		Person person1=new Person("ben");
+		fullList.addNewPerson(person1);
 		fullList.addNewPerson(new Person("ten"));
 		fullList.addNewPerson(new Person("bill"));
 		fullList.addNewPerson(new Person("xian"));
@@ -16,13 +17,17 @@ public class TestFullList {
 		fullList.addNewPerson(new Person("apple"));
 		fullList.addNewPerson(new Person("Tom"));
 		fullList.addNewPerson(new Person("∞¢“Ã"));
+		fullList.addNewPerson(new Person("zeze"));
 		fullList.addNewPerson(new Person("xj"));
 		fullList.addNewPerson(new Person("Œ‚ºŒ»Û"));
+		fullList.removePerson(person1);
+		
 		for (Person person : fullList.getPersonList()) {
 			System.out.println(person.getName());
 		}
 		for (int person : fullList.getHeader()) {
 			System.out.println(person);
 		}
+		System.out.println(fullList.getPersonList().get(fullList.getHeader()[26]).getName());
 	}
 }
