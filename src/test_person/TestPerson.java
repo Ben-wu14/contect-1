089036b8b400980048e494131c65d0ca06529c9c
ben <845687624@qq.com>
2017-03-18 08:51:08 +0800

    添加自动合成拼音功能

--- src/test_person/TestPerson.java
+++ src/test_person/TestPerson.java
@@ -4,7 +4,7 @@ public class TestPerson {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Person ben_ten = new Person("ben_ten");
+		Person ben_ten = new Person("ben_ten_Œ‚ºŒ»Û");
 		ben_ten.setPhoneNumber("13660081839");
 		ben_ten.setTel("87865467");
 		ben_ten.setCompany("SCAU");
@@ -16,6 +16,8 @@ public class TestPerson {
 		ben_ten.setBirthday(date);
 		ben_ten.setEmail("845687624@qq.com");
 		ben_ten.setInstantContect("phone");
+		System.out.println("Name: "+ben_ten.getName());
+		System.out.println("phoneticize: "+ben_ten.getPhoneticize());
 		System.out.println("Phone:"+ben_ten.getPhoneNumber());
 		System.out.println("Tel:"+ben_ten.getTel());
 		System.out.println("Company"+ben_ten.getCompany());
