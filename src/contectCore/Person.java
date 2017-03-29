7e2a9c9a5f61abff7e84ca0637991e17d79ec433
ben <845687624@qq.com>
2017-03-29 11:30:14 +0800

    add two more phoneticize

--- src/contectCore/Person.java
+++ src/contectCore/Person.java
@@ -6,6 +6,8 @@ import net.sourceforge.pinyin4j.PinyinHelper;
 public class Person implements Save{
 	private String name;
 	private String phoneticize;
+	private String midfPontic;
+	private String shortPontic;
 	private String phoneNumber;
 	private String tel;
 	private String company;
@@ -20,7 +22,7 @@ public class Person implements Save{
 	public Person(String name) {
 		// TODO Auto-generated constructor stub
 		this.name=name;
-		setPhoneticize(operatePhoneticize(name));
+		operatePhoneticize(name);
 	}
 	//各项内容的get和set方法
 	public String getName() {
@@ -95,30 +97,52 @@ public class Person implements Save{
 	public void setPhoneticize(String phoneticize) {
 		this.phoneticize = phoneticize;
 	}
+	public String getShortPontic() {
+		return shortPontic;
+	}
+	public void setShortPontic(String shortPontic) {
+		this.shortPontic = shortPontic;
+	}
+	public String getMidfPontic() {
+		return midfPontic;
+	}
+	public void setMidfPontic(String midfPontic) {
+		this.midfPontic = midfPontic;
+	}
 	//把联系人加入列表ListOfGroup中所有组别中
 	public void addToGroups(List<Group> list){
 				for (Group group : list){
 					group.addPerson(this);
 				}
 	}
-	public String operatePhoneticize(String name){
+	public void operatePhoneticize(String name){
 		//输入用户名字，输出汉语拼音（吴---wu2）,如果不是汉语，则不改变字符
 		StringBuilder pyBuilder=new StringBuilder();
+		StringBuilder stBuilder=new StringBuilder();
+		StringBuilder mdBuilder=new StringBuilder();
 		String temp=null;
 		for (int i = 0; i < name.length(); i++) {
 			temp=operateChar(name.charAt(i));
 			if(temp!=null){
 				pyBuilder.append(temp);
+				stBuilder.append(temp.charAt(0));
+				mdBuilder.append(temp.substring(0, temp.length()-1));
 			}else{
 				if (Character.isUpperCase(name.charAt(i))) {
 					pyBuilder.append(Character.toLowerCase(name.charAt(i)));
+					stBuilder.append(Character.toLowerCase(name.charAt(i)));
+					mdBuilder.append(Character.toLowerCase(name.charAt(i)));
 				} else {
 					pyBuilder.append(name.charAt(i));
+					stBuilder.append(name.charAt(i));
+					mdBuilder.append(name.charAt(i));
 				}
 				
 			}
 		}
-		return pyBuilder.toString();
+		shortPontic=stBuilder.toString();
+		phoneticize=pyBuilder.toString();
+		midfPontic=mdBuilder.toString();
 	}
 	private String operateChar(char c){
 		//单个字符转换，辅助operatePhoneticize的实现
